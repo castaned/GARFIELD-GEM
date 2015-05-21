8a50a2db734c2e27c91e86d64d0da9fea84e86d5
Alfredo Castaneda <castaned@cern.ch>
2015-05-21 11:50:31 +0300

    removing files

--- test2/initialize.sh
+++
@@ -1,8 +0,0 @@
-module load intel/compiler/13.0.1/64bit
-module load intel/mkl/11.0u1/64bit
-module load intel/mpi/4.1/64bit
-
-export GARFIELD_HOME=/panfs/vol/HEP/SW/garfield/
-export HEED_DATABASE=$GARFIELD_HOME/Heed/heed++/database/
-export ROOTSYS=/panfs/vol/HEP/SW/root/
-source $ROOTSYS/bin/thisroot.sh 
