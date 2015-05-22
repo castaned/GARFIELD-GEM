dda55c59dba95cce558553a2c9c4a5d2f6677d97
Alfredo Castaneda <castaned@cern.ch>
2015-05-23 00:16:51 +0300

    updated

--- triple_gem/gem.C
+++ triple_gem/gem.C
@@ -105,25 +105,25 @@ gas->SetComposition("ar", 70., "co2", 30.);
   drift->SetSensor(sensor);
   drift->SetDistanceSteps(2.e-4);
   
-  //Canvas to display the drift lines and the field
-  TCanvas * c = new TCanvas("c", "c", 1000, 20, 700, 500);
-  ViewDrift* viewdrift = new ViewDrift();
-  //  viewdrift->SetArea(-0.01, -0.4, 0., 0.,0.3, 0.02);
-  viewdrift->SetArea(-2*pitch, -0.4, -.048, 2*pitch, 0.3, 0.048);
-  viewdrift->SetClusterMarkerSize(0.08);
-  viewdrift->SetCollisionMarkerSize(0.1);
+  // //Canvas to display the drift lines and the field
+  // TCanvas * c = new TCanvas("c", "c", 1000, 20, 700, 500);
+  // ViewDrift* viewdrift = new ViewDrift();
+  // //  viewdrift->SetArea(-0.01, -0.4, 0., 0.,0.3, 0.02);
+  // viewdrift->SetArea(-2*pitch, -0.4, -.048, 2*pitch, 0.3, 0.048);
+  // viewdrift->SetClusterMarkerSize(0.08);
+  // viewdrift->SetCollisionMarkerSize(0.1);
 
-  // viewdrift->SetCanvas(c);
-  // new
-  ViewFEMesh* meshView = new ViewFEMesh();
-  meshView->SetComponent(fm);
-  meshView->SetArea(-2*pitch, -0.4, -.048, 2*pitch, 0.3, 0.04);
-  meshView->SetFillMesh(true);
-  //  meshView->SetCanvas(c);
+  // // viewdrift->SetCanvas(c);
+  // // new
+  // ViewFEMesh* meshView = new ViewFEMesh();
+  // meshView->SetComponent(fm);
+  // meshView->SetArea(-2*pitch, -0.4, -.048, 2*pitch, 0.3, 0.04);
+  // meshView->SetFillMesh(true);
+  // //  meshView->SetCanvas(c);
   
-  aval->EnablePlotting(viewdrift);
-  drift->EnablePlotting(viewdrift);
-  // viewdrift->Plot();
+  // aval->EnablePlotting(viewdrift);
+  // drift->EnablePlotting(viewdrift);
+  // // viewdrift->Plot();
   
   // open a root file to write results
   TFile* f = new TFile("test.root","RECREATE");
@@ -138,7 +138,7 @@ gas->SetComposition("ar", 70., "co2", 30.);
     double y0 = 0.025; 
     double z0 = 0.006;
     double t0 = 0.;  // time
-    double e0 = 0.5; //energy
+    double e0 =1.0; //energy   0.5keV
        
     //make the avalanche for the defined electron
     aval->AvalancheElectron(x0, y0, z0, t0, e0, 0., 0., 0.);
